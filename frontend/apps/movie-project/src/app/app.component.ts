51f505dafebf44563791d55706ff8e1d732035bf
KEREM DANIŞMAZ <danismazkerem@outlook.com>
2021-10-06 17:51:42 +0300

    add route system for the front end angular.

--- frontend/apps/movie-project/src/app/app.component.ts
+++ frontend/apps/movie-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'client-root',
   templateUrl: './app.component.html',
---
+++ frontend/apps/movie-project/src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'client-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+}
